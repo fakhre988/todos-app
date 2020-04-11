d5cb2b387626be199e35d507238b7fa176a90445
fakhre988 <aitouahaqi.fakhereddine@emsi-edu.ma>
2020-04-12 00:49:20 +0100

    Added todo

---
+++ resources/views/todos/create.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<h1 class="text-center my-5">Create Todos</h1>
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card card-default">
+            <div class="card-header">Create a new Todo</div>
+            <div class="card-body">
+                @if($errors->any())
+                    <div class="alert alert-danger">
+                        <ul class="list-group">
+                            @foreach($errors->all() as $error)
+                                <li class="list-group-item">
+                                    {{ $error }}
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <form action="/store-todos" method="POST">
+                    @csrf <!--bach l form t submitta l server t laravel bla biha maymkench doz -->
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Name" name="name">
+                    </div>
+                    <div class="form-group">
+                        <textarea name="description" placeholder="Description" cols="5" rows="5" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group text-center">
+                        <button type="submit" class="btn btn-success">Create</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
